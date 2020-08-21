b319ad16246e4bad3e8aa9064707a77c9d4a566c
Gaurav Pore <poregaurav13@gmail.com>
2020-08-21 22:11:48 +0530

    Add files via upload

---
+++ Salary Prediction (Regression).py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[30]:
+
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+# In[31]:
+
+
+dataset = pd.read_csv('C:/Users/Gaurav/Downloads/salary_data.csv')
+dataset.head(30)
+
+
+# In[32]:
+
+
+#Plotting scatter_matrix of dataframe
+pd.plotting.scatter_matrix(dataset)
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 1].values
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
+
+# Fitting Simple Linear Regression to the Training set
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+#Estimating accuracy 
+regressor.score(X_test, y_test)
+
+
+# In[33]:
+
+
+# Predicting the Test set results
+y_pred = regressor.predict(X_test)
+print (y_pred)
+
+
+# In[34]:
+
+
+# Visualising the Training set results
+plt.scatter(X_train, y_train, color = 'red')
+plt.plot(X_train, regressor.predict(X_train), color = 'blue')
+plt.title('Salary vs Experience (Training set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
+
+
+# In[35]:
+
+
+# Visualising the Test set results
+plt.scatter(X_test, y_test, color = 'red')
+plt.plot(X_train, regressor.predict(X_train), color = 'blue')
+plt.title('Salary vs Experience (Test set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
+
+
+# In[36]:
+
+
+#Evaluation
+from sklearn import metrics
+print('Mean Absolute Error:', metrics.mean_absolute_error(y_test, y_pred))  
+print('Mean Squared Error:', metrics.mean_squared_error(y_test, y_pred))  
+print('Root Mean Squared Error:', np.sqrt(metrics.mean_squared_error(y_test, y_pred)))
+
+
+# In[ ]:
+
+
+
+
